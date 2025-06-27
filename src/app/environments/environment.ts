9f4863b7db9e9492e23b2ebd99b142a0a6d66b62
Sensations Solutions <111495604+sensationssolutions@users.noreply.github.com>
2025-06-27 17:32:05 +0530

    style fixes

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,11 +1,11 @@
 export const environment = {
-  apiUrl: 'https://api.stadshaven.com/public/api',
-  frontendUrl: 'https://dashboard.stadshaven.com',
-  assetsUrl: 'https://api.stadshaven.com/public/',
+  // apiUrl: 'https://api.stadshaven.com/public/api',
+  // frontendUrl: 'https://dashboard.stadshaven.com',
+  // assetsUrl: 'https://api.stadshaven.com/public/',
 
-  // apiUrl: 'http://127.0.0.1:8000/api',
-  // frontendUrl: 'http://localhost:57651',
-  // assetsUrl: 'http://127.0.0.1:8000/',
+  apiUrl: 'http://127.0.0.1:8000/api',
+  frontendUrl: 'http://localhost:57651',
+  assetsUrl: 'http://127.0.0.1:8000/',
   
 };
 
